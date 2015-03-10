7655e097bbd91886b069af67a73f29c1942aa6a7
Adam Alton <adamalton@gmail.com>
2015-03-10 15:51:08 +0000

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+PACKAGES = find_packages()
+
+setup(name='CSPReports',
+      version='1.0',
+      description=(
+        "A Django app for handling reports from web browsers of violations of your website's "
+        "content security policy."
+    ),
+      author='Adam Alton - Potato London Ltd',
+      author_email='adamalton@gmail.com',
+      url='https://github.com/adamalton/django-csp-reports',
+      packages=PACKAGES,
+      include_package_data=True,
+     )
