1d94097f26d044cc73f1a376c1c3d60da437a48a
Adam Alton <adamalton@gmail.com>
2015-03-17 14:49:32 +0000

    Adding mock.py as a dependency for tests.

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ from setuptools import find_packages
 
 PACKAGES = find_packages()
 
-setup(name='CSPReports',
-      version='1.0',
-      description=(
+EXTRAS = {
+    "test": ["mock"],
+}
+
+setup(
+    name='CSPReports',
+    version='1.0',
+    description=(
         "A Django app for handling reports from web browsers of violations of your website's "
         "content security policy."
     ),
-      author='Adam Alton - Potato London Ltd',
-      author_email='adamalton@gmail.com',
-      url='https://github.com/adamalton/django-csp-reports',
-      packages=PACKAGES,
-      include_package_data=True,
-     )
+    author='Adam Alton - Potato London Ltd',
+    author_email='adamalton@gmail.com',
+    url='https://github.com/adamalton/django-csp-reports',
+    packages=PACKAGES,
+    include_package_data=True,
+    # dependencies
+    extras_require=EXTRAS,
+    tests_require=EXTRAS['test'],
+    )
