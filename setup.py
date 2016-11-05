7c85ca0dcdb83a2659dc919c202cfc8836ef33b9
Adam Alton <adamalton@gmail.com>
2016-11-05 16:10:16 +0000

    Add setup.cfg and make various setup/licence tweaks for release to PyPI.

--- setup.py
+++ setup.py
@@ -19,9 +19,12 @@ setup(
     author='Adam Alton',
     author_email='adamalton@gmail.com',
     url='https://github.com/adamalton/django-csp-reports',
+    download_url='https://github.com/adamalton/django-csp-reports/tarball/1.0',
     packages=PACKAGES,
     include_package_data=True,
     # dependencies
     extras_require=EXTRAS,
     tests_require=EXTRAS['test'],
+    keywords=['django', 'csp', 'content security policy'],
+    classifiers=['License :: OSI Approved :: MIT License', 'Framework :: Django']
     )
