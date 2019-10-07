cdc6f34d0e6ce377222ee35608ec5644c6a0fb48
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-10-07 10:02:54 +0200

    Drop admin autodiscover

--- cspreports/urls.py
+++ cspreports/urls.py
@@ -1,12 +1,8 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.contrib import admin
 
 from .views import report_csp
-
-admin.autodiscover()
-
 
 urlpatterns = [
     url(r'^report/$', report_csp, name='report_csp'),
