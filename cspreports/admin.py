6ad1dca901ba5ecb52f437791a76542921de005a
Ilya Zhivetiev <i.zhivetiev@gmail.com>
2022-02-04 17:09:44 +0800

    Fix: support for the allow_tags attribute is removed

--- cspreports/admin.py
+++ cspreports/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from cspreports.models import CSPReport
 
@@ -9,7 +10,7 @@ class CSPReportAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified', 'json_as_html')
 
     def json_as_html(self, instance):
-        return "<br />" + instance.json_as_html()
+        return format_html("<br />" + instance.json_as_html())
 
     def document_uri(self, instance):
         return instance.data.get('csp-report', {}).get('document-uri')
@@ -18,7 +19,6 @@ class CSPReportAdmin(admin.ModelAdmin):
         return instance.data.get('csp-report', {}).get('blocked-uri')
 
     json_as_html.short_description = "Report"
-    json_as_html.allow_tags = True
 
 
 admin.site.register(CSPReport, CSPReportAdmin)
