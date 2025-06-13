3d68796ee31dd26a98dc127a719fd5d708951aca
Adam Alton <270255+adamalton@users.noreply.github.com>
2025-06-13 12:07:51 +0100

    Fix the command to run tests in tox.ini

--- runtests.py
+++ runtests.py
@@ -4,6 +4,7 @@
 This file is required to run Django tests in tox.
 Based on http://joebergantine.com/articles/reusable-django-application-travis-tox/
 """
+
 import os
 import sys
 import django
@@ -13,13 +14,13 @@ from django.test.utils import get_runner
 
 
 def runtests():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'cspreports.tests.settings'
+    os.environ["DJANGO_SETTINGS_MODULE"] = "cspreports.tests.settings"
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(['cspreports'])
+    failures = test_runner.run_tests(["cspreports"])
     sys.exit(bool(failures))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     runtests()
